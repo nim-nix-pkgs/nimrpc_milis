e518ded29398245912110ef7eaf1aeaafb1993ee
hugosenari <hugosenari@gmail.com>
2022-03-12 04:31:29 -0300

    chore: re index nimrpc_milis

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''RPC implementation for Nim based on msgpack4nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimrpc_milis-master".url = "path:./master";
+  inputs."nimrpc_milis-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimrpc_milis-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
